a768c8d8858d4376309628c18ad8ec00fa049018
Young <iamduyang@hust.edu.cn>
2016-10-09 17:10:00 +0800

    adding a script aimming at changing affix of a file

---
+++ changeAffixName.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-  
+
+import os,sys
+
+def  changeFileName(sourceName,targetname):
+	lenSourceName = len(sourceName)
+	remainPartFileName= ""
+	oriFilename=sourceName
+	targetFilename=targetname
+	nameChangedNumeber=0
+
+
+	for filename in os.listdir(r'./'):
+		if filename[0]=='.':
+			continue
+
+		lenFilename=len(filename)
+		if lenFilename < (lenSourceName+1):	 
+			continue
+
+		filenameAffix=filename[(lenFilename-lenSourceName):lenFilename]
+		 
+		if filenameAffix==sourceName:
+			remainPartFileName=filename[0:(lenFilename-lenSourceName)]
+
+			oriFilename=remainPartFileName+oriFilename
+			targetFilename=remainPartFileName+targetname
+
+			operationConmmand = "mv "+oriFilename+" "+targetFilename
+			os.system(operationConmmand)
+			nameChangedNumeber = nameChangedNumeber+1
+
+	return nameChangedNumeber
+		 
+
+
+
+
+if __name__ == '__main__':
+	commandArgs=sys.argv
+
+	try:
+		sourceName=commandArgs[1]
+		targetname=commandArgs[2]
+		 
+		nameChangedNumeber= changeFileName(sourceName,targetname)
+		print "Done! "+str(nameChangedNumeber)+" of files have been chaneged"
+	except:
+		print "Error: Usage \'python changeAffixName.py BEFOREnAME AFTERnAME"
